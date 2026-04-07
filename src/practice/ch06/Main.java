27ec9b33e206fd16b727dd807ed8239ec74a0437
mingging227 <mingging227@gmail.com>
2026-04-07 21:58:49 +0900

    0407

--- src/practice/ch06/Main.java
+++ src/practice/ch06/Main.java
@@ -1,8 +1,36 @@
 package practice.ch06;
 
+import practice.ch06.framework.Manager;
+
 public class Main {
     public static void main(String[] args) {
-        MessageBox m1 = new MessageBox('@');
-        m1.use("Hello, world.");
+        // // 원본을 이용
+        // MessageBox m1 = new MessageBox('@');
+        // m1.use("Hello, world.");
+
+        // // 복제본을 이용
+        // m1.createCopy().use("Hello, world.");
+
+        // // 원본을 이용
+        // UnderlinePen u1 = new UnderlinePen('-');
+        // u1.use("Hello, world.");
+
+        // // 복제본을 이용
+        // u1.createCopy().use("Hello, world.");
+    
+        Manager manager = new Manager();
+        
+        MessageBox mbox1 = new MessageBox('*');
+        manager.register("star box", mbox1);
+
+        UnderlinePen upen1 = new UnderlinePen('-');
+        manager.register("underline pen", upen1);
+
+        
+        MessageBox mbox2 = new MessageBox('/');
+        manager.register("slash box", mbox2);
+
+        // 복제 객체를 이용
+        manager.create("star box").use("Hello, world.");
     }
 }
